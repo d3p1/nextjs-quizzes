edc425ba9c3d6b951d2412a3b0c03a0f738cec03
C. M. de Picciotto <d3p1@d3p1.dev>
2025-02-28 16:38:14 +0000

    feat: add loader [#1]

---
+++ app/loading.tsx
@@ -0,0 +1,7 @@
+/**
+ * @description Quiz page loader
+ * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
+ */
+export default function Loading() {
+  return <div>Loading...</div>
+}
