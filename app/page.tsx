503cac085848223137f591388fcfa567726b411e
C. M. de Picciotto <d3p1@d3p1.dev>
2025-02-28 15:29:11 +0000

    feat: add quiz list to home page [#1]

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,25 @@
  * @description Home page
  * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
  */
+import data from '@/data'
+
+function QuizList() {
+  return (
+    <ul className="flex flex-col justify-center items-center m-3">
+      {data.map((quiz, i) => (
+        <li key={i} className="mt-1">
+          {quiz.title}
+        </li>
+      ))}
+    </ul>
+  )
+}
+
 export default function Home() {
   return (
     <div className="m-2">
       <h1 className="text-2xl font-bold">Quizzes</h1>
+      <QuizList />
     </div>
   )
 }
